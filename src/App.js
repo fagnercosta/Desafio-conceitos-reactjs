1473ba0d18c73c9dda9d859cae1c74085d2259eb
Fagner Costa <2192525@ifam>
2022-11-12 20:19:51 -0400

    Correção

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from "react";
+import React,{useEffect, useState} from "react";
+
+import api  from './services/api';
 
 import "./styles.css";
 
 function App() {
-  async function handleAddRepository() {
-    // TODO
+  const [repositories, setRepositories]= useState([]);
+
+  useEffect(()=>{
+    api.get('/repositories').then(response=>{
+        setRepositories(response.data);
+    });
+},[]);
+
+async function handleAddRepository() {
+        const response = await api.post('/repositories',{
+                title:'Fagner',
+                url:'https://github.com/rocketseat-education/gostack-template-conceitos-nodejs',
+                techs:['Node JS']
+        });
+
+        setRepositories([...repositories, response.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+   
+        await api.delete(`repositories/${id}`);
+        setRepositories(repositories.filter(repository=>repository.id!==id));
+        
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+            { repositories.map(repository =>(
+                     <li key={repository.id}>
+                        {repository.title}
+          
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+                     <button onClick={() => handleRemoveRepository(1)}>
+                       Remover
+                     </button>
+                   </li>
+            ))}
+       
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
